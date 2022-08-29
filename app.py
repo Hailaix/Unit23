6a047afbf104452a3d0263d31f41af09ee8306d8
NHower <howernickoli@gmail.com>
2022-08-29 17:42:41 -0400

    routes complete + further study

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -22,13 +22,26 @@ def index():
 @app.route("/users")
 def users():
     """displays all users"""
-    users = User.query.all()
+    users = User.query.order_by(User.last_name.asc(),User.first_name.asc()).all()
     return render_template("users.html", users=users)
 
 @app.route("/users/new")
 def userform():
     """form page to create a new user"""
     return render_template("userform.html")
+
+@app.route("/users/new", methods=["POST"])
+def userform_submit():
+    """create a new user on submit of new user form"""
+    first_name = request.form["first_name"].strip()
+    last_name = request.form["last_name"].strip()
+    image_url = request.form["image_url"].strip()
+    if not image_url:
+        image_url = None
+    newuser = User(first_name=first_name,last_name=last_name,image_url=image_url)
+    db.session.add(newuser)
+    db.session.commit()
+    return redirect("/users")
 
 @app.route("/users/<int:user_id>")
 def user_details(user_id):
@@ -37,3 +50,36 @@ def user_details(user_id):
     curruser = User.query.get_or_404(user_id)
     return render_template("userpage.html", user=curruser)
 
+@app.route("/users/<int:user_id>/edit")
+def edit_user(user_id):
+    """edit the details of the user with id user_id"""
+
+    curruser = User.query.get_or_404(user_id)
+    return render_template("usereditform.html",user=curruser)
+
+@app.route("/users/<int:user_id>/edit", methods=["POST"])
+def edit_user_submit(user_id):
+    """edits the info of user with id user_id"""
+    new_fname = request.form["first_name"].strip()
+    new_lname = request.form["last_name"].strip()
+    new_img_url = request.form["image_url"].strip()
+    
+    curruser = User.query.get_or_404(user_id)
+    
+    if new_fname:
+        curruser.first_name = new_fname
+    if new_lname:
+        curruser.last_name = new_lname
+    if new_img_url:
+        curruser.image_url = new_img_url
+        
+    db.session.add(curruser)
+    db.session.commit()
+    return redirect(f'/users/{user_id}')
+
+@app.route("/users/<int:user_id>/delete", methods=["POST"])
+def delete_user(user_id):
+    """delete the user with id user_id"""
+    User.query.filter_by(id=user_id).delete()
+    db.session.commit()
+    return redirect("/users")
--- models.py
+++ models.py
@@ -16,3 +16,13 @@ class User(db.Model):
     first_name = db.Column(db.String(32), nullable=False)
     last_name = db.Column(db.String(32), nullable=False)
     image_url = db.Column(db.String, nullable=False, default="https://cdn.iconscout.com/icon/free/png-256/user-3114475-2598167.png")
+
+    # full name property
+    def _get_full_name(self):
+        """returns the first and last name of the user"""
+        return f"{self.first_name} {self.last_name}"
+
+    full_name = property(
+        fget=_get_full_name,
+        doc="Full name of the user"
+    )
